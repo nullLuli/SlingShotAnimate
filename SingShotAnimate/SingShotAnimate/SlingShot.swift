ea73d6cd5736a2ff2a45345a55924365875325b9
nullLuli <2323598470@qq.com>
2019-07-02 17:53:01 +0800

    控制点调整

--- SingShotAnimate/SingShotAnimate/SlingShot.swift
+++ SingShotAnimate/SingShotAnimate/SlingShot.swift
@@ -32,55 +32,41 @@ class SlingShotController: UIViewController {
         switch gesture.state {
         case .began:
             beginPoint = gesture.location(in: view)
-//            debugPrint("begin: \(gesture.location(in: view))")
         case .changed:
             let currentPoint = gesture.location(in: view)
             if let begin = beginPoint {
                 changeLine(begin: begin, current: currentPoint)
             }
-//            debugPrint("changed: \(gesture.location(in: view))")
-        case .possible:
-            ()
-//            debugPrint("possible: \(gesture.location(in: view))")
+//        case .ended:
+//            let currentPoint = gesture.location(in: view)
+//            if let begin = beginPoint {
+//                changeLine(begin: begin, current: currentPoint)
+//            }
         default:
             ()
         }
     }
     
     func changeLine(begin: CGPoint, current: CGPoint) {
-        let leftContr1Y = (current.y - begin.y) / 3 + beginY - yDeviation
-        let leftContr1X = UIScreen.main.bounds.width / 6
+        let leftContrY = (current.y - begin.y) / 2 + beginY + yDeviation
+        let leftContrX = UIScreen.main.bounds.width / 4
         
-        let leftContr2Y = (current.y - begin.y) * 2 / 3 + beginY - yDeviation
-        let leftContr2X = UIScreen.main.bounds.width / 3
+        let rightContrY = leftContrY
+        let rightContrX = UIScreen.main.bounds.width - leftContrX
         
-        let rightContr2Y = leftContr2Y
-        let rightContr2X = UIScreen.main.bounds.width - leftContr2X
-        
-        let rightContr1Y = leftContr1Y
-        let rightContr1X = UIScreen.main.bounds.width - leftContr1X
-        
-        let movePoint = CGPoint(x: UIScreen.main.bounds.width / 2, y: beginY + current.y - begin.y)
-        
-        slingShotView.leftContr1 = CGPoint(x: leftContr1X, y: leftContr1Y)
-        slingShotView.leftContr2 = CGPoint(x: leftContr2X, y: leftContr2Y)
-        slingShotView.rightContr2 = CGPoint(x: rightContr2X, y: rightContr2Y)
-        slingShotView.rightContr1 = CGPoint(x: rightContr1X, y: rightContr1Y)
-        slingShotView.movePoint = movePoint
+        slingShotView.leftContr = CGPoint(x: leftContrX, y: leftContrY)
+        slingShotView.rightContr = CGPoint(x: rightContrX, y: rightContrY)
         
         slingShotView.setNeedsDisplay()
     }
 }
 
-let beginY: CGFloat = 100
+let beginY: CGFloat = 400
 let yDeviation: CGFloat = 50
 
 class SlingShotView: UIView {
-    var leftContr1: CGPoint = CGPoint.zero
-    var leftContr2: CGPoint = CGPoint.zero
-    var rightContr2: CGPoint = CGPoint.zero
-    var rightContr1: CGPoint = CGPoint.zero
-    var movePoint: CGPoint?
+    var leftContr: CGPoint = CGPoint(x: UIScreen.main.bounds.width / 3, y: beginY)
+    var rightContr: CGPoint = CGPoint(x: UIScreen.main.bounds.width * 2 / 3, y: beginY)
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -92,20 +78,15 @@ class SlingShotView: UIView {
         
         let path = UIBezierPath.init()
         path.move(to: beginPoint)
-        if let movePoint = movePoint {
-            path.addCurve(to: movePoint, controlPoint1: leftContr1, controlPoint2: leftContr2)
-            path.addCurve(to: endPoint, controlPoint1: rightContr2, controlPoint2: rightContr1)
-        } else {
-            path.addLine(to: endPoint)
-        }
+        path.addCurve(to: endPoint, controlPoint1: leftContr, controlPoint2: rightContr)
         path.lineWidth = 2
         
         path.stroke()
         
-        let leftContrPath = UIBezierPath(rect: CGRect(origin: leftContr1, size: CGSize(width: 2, height: 2)))
+        let leftContrPath = UIBezierPath(rect: CGRect(origin: leftContr, size: CGSize(width: 2, height: 2)))
         leftContrPath.stroke()
         
-        let leftContrPath2 = UIBezierPath(rect: CGRect(origin: leftContr2, size: CGSize(width: 2, height: 2)))
-        leftContrPath2.stroke()
+        let rightContrPath = UIBezierPath(rect: CGRect(origin: rightContr, size: CGSize(width: 2, height: 2)))
+        rightContrPath.stroke()
     }
 }
