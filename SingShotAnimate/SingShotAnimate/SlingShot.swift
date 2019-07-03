82a1039aba232d1be88c9767c0070771772172fb
nullLuli <2323598470@qq.com>
2019-07-03 12:01:34 +0800

    使用CAKeyFrameAnimation进行弹回多动画

--- SingShotAnimate/SingShotAnimate/SlingShot.swift
+++ SingShotAnimate/SingShotAnimate/SlingShot.swift
@@ -14,7 +14,7 @@ class SlingShotController: UIViewController {
     let slingShotView: SlingShotView = SlingShotView()
     
     //数据
-    var beginPoint: CGPoint?
+    var beginPoint: CGPoint = CGPoint(x: UIScreen.main.bounds.width / 2, y: beginY)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +23,8 @@ class SlingShotController: UIViewController {
         view.addSubview(slingShotView)
         slingShotView.frame = view.bounds
         slingShotView.backgroundColor = UIColor.white
-        slingShotView.updatePath(leftContr: beginLeftContr, rightContr: beginRightContr)
+        let path = getPathFromContr(leftContr: beginLeftContr, rightContr: beginRightContr)
+        slingShotView.shapeLayer.path = path
         
         let pan = UIPanGestureRecognizer(target: self, action: #selector(pan(gesture:)))
         view.addGestureRecognizer(pan)
@@ -31,30 +32,63 @@ class SlingShotController: UIViewController {
     
     @objc func pan(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
-        case .began:
-            beginPoint = gesture.location(in: view)
         case .changed:
             let currentPoint = gesture.location(in: view)
-            if let begin = beginPoint {
-                changeLine(begin: begin, current: currentPoint)
-            }
+            let (leftContr, rightContr) = getContr(with: beginPoint, end: currentPoint)
+            let path = getPathFromContr(leftContr: leftContr, rightContr: rightContr)
+            slingShotView.shapeLayer.path = path
         case .ended:
-            slingShotView.updatePath(leftContr: beginLeftContr, rightContr: beginRightContr)
+            slingShotView.shapeLayer.path = getPathFromContr(leftContr: beginLeftContr, rightContr: beginRightContr)
+
+            let currentPoint = gesture.location(in: view)
+            animateSlingBack(end: currentPoint)
         default:
             ()
         }
     }
     
-    func changeLine(begin: CGPoint, current: CGPoint) {
-        let leftContrY = (current.y - begin.y) / 2 + beginY + yContrDevi(diffY: (current.y - begin.y))
+    func animateSlingBack(end: CGPoint) {
+        let path1 = getKeyPath(with: beginPoint, end: end, precent: 1)
+        let path2 = getKeyPath(with: beginPoint, end: end, precent: 0.4)
+        let path3 = getKeyPath(with: beginPoint, end: end, precent: -0.2)
+        let path4 = getKeyPath(with: beginPoint, end: end, precent: 0.1)
+        let path5 = getKeyPath(with: beginPoint, end: end, precent: 0)
+        
+        let animate = CAKeyframeAnimation(keyPath: "path")
+        animate.values = [path1, path2, path3, path4, path5]
+        animate.isRemovedOnCompletion = true
+        animate.duration = 1
+        slingShotView.shapeLayer.add(animate, forKey: nil)
+    }
+    
+    func getKeyPath(with begin: CGPoint, end: CGPoint, precent: CGFloat) -> CGPath {
+        let newY = (end.y - begin.y) * precent + begin.y
+        let newPoint = CGPoint(x: end.x, y: newY)
+        let (leftContr, rightContr) = getContr(with: begin, end: newPoint)
+        return getPathFromContr(leftContr: leftContr, rightContr: rightContr)
+    }
+    
+    func getContr(with begin: CGPoint, end: CGPoint) -> (CGPoint, CGPoint) {
+        let leftContrY = (end.y - begin.y) / 2 + begin.y + yContrDevi(diffY: (end.y - begin.y))
         let leftContrX = UIScreen.main.bounds.width / 3
         
         let rightContrY = leftContrY
         let rightContrX = UIScreen.main.bounds.width - leftContrX
         
-        slingShotView.updatePath(leftContr: CGPoint(x: leftContrX, y: leftContrY), rightContr: CGPoint(x: rightContrX, y: rightContrY))
+        return (CGPoint(x: leftContrX, y: leftContrY), CGPoint(x: rightContrX, y: rightContrY))
     }
     
+    func getPathFromContr(leftContr: CGPoint, rightContr: CGPoint) -> CGPath {
+        let beginPoint: CGPoint = CGPoint(x: 0, y: beginY)
+        let endPoint: CGPoint = CGPoint(x: UIScreen.main.bounds.width, y: beginY)
+        
+        let path = UIBezierPath.init()
+        path.move(to: beginPoint)
+        path.addCurve(to: endPoint, controlPoint1: leftContr, controlPoint2: rightContr)
+        path.lineWidth = 2
+        return path.cgPath
+    }
+
     func yContrDevi(diffY: CGFloat) -> CGFloat {
         if diffY > 1 || diffY < -1 {
             let result: CGFloat = (1 / diffY) * -1 + 2 * diffY / abs(diffY)
@@ -84,20 +118,5 @@ class SlingShotView: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    func updatePath(leftContr: CGPoint, rightContr: CGPoint) {
-        shapeLayer.path = getPath(leftContr: leftContr, rightContr: rightContr)
-    }
-    
-    private func getPath(leftContr: CGPoint, rightContr: CGPoint) -> CGPath {
-        let beginPoint: CGPoint = CGPoint(x: 0, y: beginY)
-        let endPoint: CGPoint = CGPoint(x: UIScreen.main.bounds.width, y: beginY)
-
-        let path = UIBezierPath.init()
-        path.move(to: beginPoint)
-        path.addCurve(to: endPoint, controlPoint1: leftContr, controlPoint2: rightContr)
-        path.lineWidth = 2
-        return path.cgPath
     }
 }
